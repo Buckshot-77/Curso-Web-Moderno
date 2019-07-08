d3ac3f18212e9f97694a2a819b36ee6ca2755c16
Buckshot-77 <Buckshot-77@github.com>
2019-07-08 20:50:38 -0300

    dadas

--- Curso Web Moderno/Funções/arrowFunction3.js
+++ Curso Web Moderno/Funções/arrowFunction3.js
@@ -5,3 +5,12 @@ let comparaComThis = function (parametro) {
 comparaComThis(global)
 
 // O escopo global é chamado de "global" no Node, e é chamado de "window" no browser, fazendo com que seja necessário e desejável entender o contexto no qual o código está sendo executado.
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
+
+let comparaComThisArrow = parametro => console.log(this === parametro)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)

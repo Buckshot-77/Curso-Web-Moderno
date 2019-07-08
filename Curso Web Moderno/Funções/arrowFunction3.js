71b6da264de69162eb098cd2589e731ef7d4c0ad
Buckshot-77 <Buckshot-77@github.com>
2019-07-08 20:41:21 -0300

    vai

--- Curso Web Moderno/Funções/arrowFunction3.js
+++ Curso Web Moderno/Funções/arrowFunction3.js
@@ -3,3 +3,5 @@ let comparaComThis = function (parametro) {
 }
 
 comparaComThis(global)
+
+// O escopo global é chamado de "global" no Node, e é chamado de "window" no browser, fazendo com que seja necessário e desejável entender o contexto no qual o código está sendo executado.

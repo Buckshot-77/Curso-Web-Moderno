28597b8d1e567b8dd0cd24ebc0b293f4c6c28fe4
Buckshot-77 <buckshot-77@github.com>
2019-07-26 12:24:01 -0300

    callback2 revamp

--- Curso Web Moderno/Funções/callback2.js
+++ Curso Web Moderno/Funções/callback2.js
@@ -2,7 +2,7 @@ const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
 
 // Sem callback
 
-let notasBaixas = []
+const notasBaixas = []
 for (let i in notas) {
     if (notas[i] < 7){
         notasBaixas.push(notas[i]) // empurra um valor para a última casa do array
@@ -13,10 +13,10 @@ console.log(notasBaixas)
 
 
 //Com Callback
-let notasBaixas2 = notas.filter((nota) => nota < 7) // usando arrow function para praticar. O método filter tem como função percorrer o array que o chama. Seu parâmetro de entrada é uma função que inclui no vetor resultado caso a mesma resulte em verdadeiro.
+const notasBaixas2 = notas.filter((nota) => nota < 7) // usando arrow function para praticar. O método filter tem como função percorrer o array que o chama. Seu parâmetro de entrada é uma função que inclui no vetor resultado caso a mesma resulte em verdadeiro.
 console.log(notasBaixas2)
 
-let notasBaixas3 = notas.filter(function(nota){
-    return nota < 7
-})
+const NotasMenoresDoQue7 = nota => nota < 7
+
+const notasBaixas3 = notas.filter(NotasMenoresDoQue7)
 console.log(notasBaixas3)

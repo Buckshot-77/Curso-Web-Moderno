f4a2767225102e17b891bffd274e025711b249eb
Buckshot-77 <buckshot-77@github.com>
2019-07-29 21:14:36 -0300

    Closure e função construtora

--- Curso Web Moderno/Funções/tiposDeclaracao.js
+++ Curso Web Moderno/Funções/tiposDeclaracao.js
@@ -1,4 +1,5 @@
-console.log(soma(3,4)) // funciona, pois com function declaration o interpretador carrega primeiro as definições.
+console.log(soma(3, 4)) // funciona, pois com function declaration o interpretador carrega primeiro as definições.
+//console.log(sub(3,4)) // não funciona, pois o interpretador não carrega a expressão antes quando ela é declarada por function expression
 
 //function declaration
 function soma(x, y) {
@@ -9,10 +10,10 @@ function soma(x, y) {
 const sub = function (x, y) {
     return x - y
 }
-console.log(sub(3,4))
+console.log(sub(3, 4))
 
 // named function expression
 const mult = function mult(x, y) {
     return x * y
 }
-console.log(mult(3,4))
+console.log(mult(3, 4))

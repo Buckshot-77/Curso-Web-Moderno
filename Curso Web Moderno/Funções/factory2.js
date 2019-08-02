082f049f192393f49c353cbb1316d5220a2acb32
Buckshot-77 <buckshot-77@github.com>
2019-08-01 22:06:36 -0300

    callApply

--- Curso Web Moderno/Funções/factory2.js
+++ Curso Web Moderno/Funções/factory2.js
@@ -1,10 +1,11 @@
 function criarProduto(nomeProduto, precoProduto, desconto = 0.4){
     return {
         nome: nomeProduto,
-        preco: precoProduto,
+        precoOriginal: precoProduto,
         desconto,
         precoComDesconto: precoProduto - precoProduto * desconto
     }
 }
 
 console.log(criarProduto('Abacate', 40))
+console.log(criarProduto('Laptop', 3500))

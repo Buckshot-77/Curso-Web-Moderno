a6c3c2ec6860f5d4bdb10e16e27ce4b371988045
Buckshot-77 <buckshot-77@github.com>
2019-08-03 19:43:36 -0300

    oo

--- Curso Web Moderno/Objetos/oo.js
+++ Curso Web Moderno/Objetos/oo.js
@@ -1,3 +1,24 @@
 // CÓDIGO NÃO EXECUTÁVEL!
 
 // Procedural
+processamento(valor1, valor2, valor3)
+
+// Orientação a Objetos
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento()
+
+/** Princípios importantes:
+ * 1 - Abstração - Adequar os métodos à relevância dos mesmos dentro da aplicação do mundo real. Sempre que for possível usar um grau de abstração maior sem comprometer a execução, é preferível.
+ * 2 - Encapsulamento - Detalhes de implementação deve 
+ * 
+ * 
+ * 
+ */

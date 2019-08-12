a39e48c5b2881053aceb52ee1ff7f23efcc1b2ac
Buckshot-77 <Buckshot-77@github.com>
2019-08-12 11:09:08 -0300

    json

--- Curso Web Moderno/Objetos/jsonVsObj.js
+++ Curso Web Moderno/Objetos/jsonVsObj.js
@@ -10,3 +10,4 @@ console.log(JSON.stringify(obj))
 //console.log(JSON.parse("{a:1,b:2,c:3}"))
 //console.log(JSON.parse("{'a':1,'b':2,'c':3}"))
 console.log(JSON.parse('{"a": 1, "b": 2, "c": 3}'))
+//

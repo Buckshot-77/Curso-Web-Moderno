eaa4dfbacd59b6bff14c859b6ed4373c2c4e4197
Buckshot-77 <Buckshot-77@github.com>
2019-08-16 13:22:18 -0300

    conclusão array

---
+++ Curso Web Moderno/Array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Andressa', 'Haley']
+const filhos = ['Jason', 'Steve']
+// const todos = filhas + filhos;
+// console.log(todos) // concatena as strings diretamente
+
+const todos = filhas.concat(filhos, 'Bill')
+console.log(todos)
